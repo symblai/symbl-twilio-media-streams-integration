3f51f45358cdfed66baf522c33e787dcc15dc107
Subodh Jena <jenasubodh@gmail.com>
2022-10-13 11:04:44 +0530

    Formatted

--- src/symbl/SymblConnectionHelper.js
+++ src/symbl/SymblConnectionHelper.js
@@ -1,120 +1,126 @@
-const uuid = require('uuid').v4;
-const trackers = require('./trackers');
+const uuid = require("uuid").v4;
+const trackers = require("./trackers");
 
-console.log('Symbl SDK Initialized.');
+console.log("Symbl SDK Initialized.");
 class SymblConnectionHelper {
+  constructor({ speaker, handlers }) {
+    this.connection = undefined;
+    this.speaker = speaker;
+    this.handlers = handlers;
+    this._hash = uuid();
+    const { SDK } = require("@symblai/symbl-js");
+    this.sdk = new SDK();
+    console.log("Cache", this.sdk.cache.cacheStore.store);
+    (async () => {
+      return this.sdk.init({
+        appId: process.env.SYMBL_APP_ID,
+        appSecret: process.env.SYMBL_APP_SECRET,
+      });
+    })();
+  }
 
-    constructor({speaker, handlers}) {
-        this.connection = undefined;
-        this.speaker = speaker;
-        this.handlers = handlers;
-        this._hash = uuid();
-        const {SDK} = require('@symblai/symbl-js');
-        this.sdk = new SDK();
-        console.log('Cache', this.sdk.cache.cacheStore.store);
-        (async () => {
-            return this.sdk.init({
-                appId: process.env.SYMBL_APP_ID,
-                appSecret: process.env.SYMBL_APP_SECRET
-            });
-        })();
+  /**
+   * Start the connection
+   * @param id
+   * @param speaker
+   * @param insightTypes
+   * @param config
+   * @return {Promise<void>}
+   */
+  async startConnection(id, { speaker, insightTypes, config } = {}) {
+    this.speaker = speaker;
+    this.connection = await this.sdk.startRealtimeRequest({
+      id,
+      basePath: "https://api-labs.symbl.ai",
+      insightTypes: insightTypes || ["action_item", "question", "follow_up"],
+      config: {
+        meetingTitle: "My Test Meeting",
+        confidenceThreshold: 0.7,
+        timezoneOffset: 480, // Offset in minutes from UTC
+        languageCode: "en-US",
+        sampleRateHertz: 8000,
+        encoding: "MULAW",
+        sentiment: true,
+        trackers: {
+          // enableAllTrackers: true,
+          interimResults: true,
+        },
+        customVocabularyStrength: [
+          { text: "Brittany" },
+          { text: "Toshish" },
+          { text: "QR Code" },
+          { text: "Sunday" },
+        ],
+        redaction: {
+          // Enable identification of PII/PCI information
+          identifyContent: true, // By default false
+          // Enable redaction of PII/PCI information
+          redactContent: false, // By default false
+          // Use custom string "[PII_PCI_ENTITY]" to replace PII/PCI information with
+          redactionString: "*****", // By default ****
+        },
+        ...config,
+      },
+      trackers,
+      speaker: this.speaker,
+      handlers: {
+        onSpeechDetected: this.onSpeechDetected.bind(this),
+        onMessageResponse: this.onMessage.bind(this),
+        onInsightResponse: this.onInsight.bind(this),
+      },
+    });
+    return this.connection;
+  }
 
+  sendAudio(data, encoding = "none") {
+    if (encoding !== "none") {
+      const buffer = Buffer.from(data, encoding);
+      this.connection.sendAudio(buffer);
+    } else {
+      this.connection.sendAudio(data);
     }
+  }
 
-    /**
-     * Start the connection
-     * @param id
-     * @param speaker
-     * @param insightTypes
-     * @param config
-     * @return {Promise<void>}
-     */
-    async startConnection(id, {speaker, insightTypes, config} = {}) {
-        this.speaker = speaker;
-        this.connection = await this.sdk.startRealtimeRequest({
-            id,
-            basePath: 'https://api-labs.symbl.ai',
-            insightTypes: insightTypes || ["action_item", "question", "follow_up"],
-            config: {
-                meetingTitle: 'My Test Meeting',
-                confidenceThreshold: 0.7,
-                timezoneOffset: 480, // Offset in minutes from UTC
-                languageCode: "en-US",
-                sampleRateHertz: 8000,
-                encoding: 'MULAW',
-                sentiment: true,
-                trackers: {
-                    // enableAllTrackers: true,
-                    interimResults: true
-                },
-                customVocabularyStrength: [
-                    {text: 'Brittany'},
-                    {text: 'Toshish'},
-                    {text: 'QR Code'},
-                    {text: 'Sunday'}
-                ],
-                "redaction": {
-                    // Enable identification of PII/PCI information
-                    "identifyContent": true, // By default false
-                    // Enable redaction of PII/PCI information
-                    "redactContent": false, // By default false
-                    // Use custom string "[PII_PCI_ENTITY]" to replace PII/PCI information with
-                    "redactionString": "*****" // By default ****
-                },
-                ...config
-            },
-            trackers,
-            speaker: this.speaker,
-            handlers: {
-                'onSpeechDetected': this.onSpeechDetected.bind(this),
-                'onMessageResponse': this.onMessage.bind(this),
-                'onInsightResponse': this.onInsight.bind(this)
-            }
+  onSpeechDetected(data) {
+    if (this.handlers && this.handlers.onSpeechDetected) {
+      setTimeout(() => {
+        this.handlers.onSpeechDetected(data, {
+          speaker: this.speaker,
+          connection: this.connection,
         });
-        return this.connection;
+      }, 0);
     }
+  }
 
-    sendAudio(data, encoding = 'none') {
-        if (encoding !== 'none') {
-            const buffer = Buffer.from(data, encoding);
-            this.connection.sendAudio(buffer);
-        } else {
-            this.connection.sendAudio(data);
-        }
+  onMessage(data) {
+    if (this.handlers && this.handlers.onMessage) {
+      setTimeout(() => {
+        this.handlers.onMessage(data, {
+          speaker: this.speaker,
+          connection: this.connection,
+        });
+      }, 0);
     }
+  }
 
-    onSpeechDetected(data) {
-        if (this.handlers && this.handlers.onSpeechDetected) {
-            setTimeout(() => {
-                this.handlers.onSpeechDetected(data, {speaker: this.speaker, connection: this.connection});
-            }, 0);
-        }
+  onInsight(data) {
+    if (this.handlers && this.handlers.onInsight) {
+      setTimeout(() => {
+        this.handlers.onInsight(data, {
+          speaker: this.speaker,
+          connection: this.connection,
+        });
+      }, 0);
     }
+  }
 
-    onMessage(data) {
-        if (this.handlers && this.handlers.onMessage) {
-            setTimeout(() => {
-                this.handlers.onMessage(data, {speaker: this.speaker, connection: this.connection});
-            }, 0);
-        }
-    }
+  async stopConnection() {
+    return this.connection.stop();
+  }
 
-    onInsight(data) {
-        if (this.handlers && this.handlers.onInsight) {
-            setTimeout(() => {
-                this.handlers.onInsight(data, {speaker: this.speaker, connection: this.connection});
-            }, 0);
-        }
-    }
-
-    async stopConnection() {
-        return this.connection.stop();
-    }
-
-
-    get hash() {
-        return this._hash;
-    }
+  get hash() {
+    return this._hash;
+  }
 }
 
 module.exports = SymblConnectionHelper;
--- src/symbl/trackers.js
+++ src/symbl/trackers.js
@@ -1,139 +1,132 @@
 const trackers = [
-    {
-        name: "Billing Issue",
-        vocabulary: [
-            "billing issue",
-            "something wrong with my billing",
-            "I have a billing problem",
-            "I am having issue with billing",
-            "I cannot see my bill",
-            "bill is not showing up",
-            "the amount in the bill is wrong",
-            "there is something wrong with my bill",
-            "it looks like there is something wrong with my bill this month",
-
-        ]
-    },
-    {
-        name: "CreditCard Added",
-        vocabulary: [
-            "I added all my credit card details",
-            "credit card details are updated",
-            "my credit card details are up to date"
-        ]
-    },
-    {
-        name: "Issue Resolved",
-        vocabulary: [
-            "Looks like the issue is resolved",
-            "it is working now",
-            "yes I can see my bill now",
-            "issue is fixed",
-            "not running into problem",
-            "thanks for resolving this for me",
-            "thanks for fixing this issue"
-        ]
-    },
-    {
-        name: "Satisfaction",
-        vocabulary: [
-            "thank you so much",
-            "thanks for your help",
-            'thanks so much for your help',
-            "appreciate it",
-            "it is working now",
-            "that's great",
-            "that's awesome",
-            "issue is resolved",
-            "I like that",
-            "this is great",
-            "I am happy",
-            "I love how you fixed it quickly",
-            "my experience was really good",
-            "you are great",
-            "that makes me so happy"
-
-        ]
-    },
-    {
-        name: "Symbl.Politeness",
-        vocabulary: [
-            "Would you mind",
-            "Thank you for your patience",
-            "I am afraid I can't",
-            "appreciate your patience",
-            "so sorry to hear that",
-            "sorry to cut you off",
-        ]
-    },
-    {
-        name: "Symbl.Empathy",
-        vocabulary: [
-            "I understand how you feel",
-            "I wish you wouldn't have to go through",
-            "I totally agree with you",
-            "don't worry I am here to support you",
-            "that would frustrate me too",
-            "I am on your side",
-            "I understand your frustration",
-        ]
-    },
-    {
-        name: "Symbl.Greetings",
-        vocabulary: [
-            "Good morning",
-            "Hello",
-            "Hi",
-            "How's your day going?",
-            "How are you?",
-            "How are you doing?",
-            "How is it going?",
-        ]
-    },
-    {
-        name: "Symbl.Goodbyes",
-        vocabulary: [
-            "Good bye",
-            "bye",
-            "bye-bye"
-        ]
-    },
-    {
-        name: "Product Experience Issue",
-        vocabulary: [
-            "it is not clear what that is",
-            "i probably missed reading everything on the screen before accepting",
-            "i got confused on the app"
-        ]
-    },
-    {
-        name: "Offered Refund",
-        vocabulary: [
-            "offer a full refund",
-            "i would like to offer a full refund",
-            "can I offer you a refund"
-        ]
-    },
-    {
-        name: "Frustration",
-        vocabulary: [
-            "I am really fed up with",
-            "I am very frustrated",
-            "I am not going to talk to another agent",
-            "I want my issue to be fixed right away",
-            "I am not happy",
-            "I am not satisfied",
-            "this is absurd",
-            "really bad service",
-            "poor customer service",
-            "annoying",
-            "I have given up",
-            "got frustrated",
-            "it is a mistake",
-            "it's a mistake"
-        ]
-    },
-
+  {
+    name: "Billing Issue",
+    vocabulary: [
+      "billing issue",
+      "something wrong with my billing",
+      "I have a billing problem",
+      "I am having issue with billing",
+      "I cannot see my bill",
+      "bill is not showing up",
+      "the amount in the bill is wrong",
+      "there is something wrong with my bill",
+      "it looks like there is something wrong with my bill this month",
+    ],
+  },
+  {
+    name: "CreditCard Added",
+    vocabulary: [
+      "I added all my credit card details",
+      "credit card details are updated",
+      "my credit card details are up to date",
+    ],
+  },
+  {
+    name: "Issue Resolved",
+    vocabulary: [
+      "Looks like the issue is resolved",
+      "it is working now",
+      "yes I can see my bill now",
+      "issue is fixed",
+      "not running into problem",
+      "thanks for resolving this for me",
+      "thanks for fixing this issue",
+    ],
+  },
+  {
+    name: "Satisfaction",
+    vocabulary: [
+      "thank you so much",
+      "thanks for your help",
+      "thanks so much for your help",
+      "appreciate it",
+      "it is working now",
+      "that's great",
+      "that's awesome",
+      "issue is resolved",
+      "I like that",
+      "this is great",
+      "I am happy",
+      "I love how you fixed it quickly",
+      "my experience was really good",
+      "you are great",
+      "that makes me so happy",
+    ],
+  },
+  {
+    name: "Symbl.Politeness",
+    vocabulary: [
+      "Would you mind",
+      "Thank you for your patience",
+      "I am afraid I can't",
+      "appreciate your patience",
+      "so sorry to hear that",
+      "sorry to cut you off",
+    ],
+  },
+  {
+    name: "Symbl.Empathy",
+    vocabulary: [
+      "I understand how you feel",
+      "I wish you wouldn't have to go through",
+      "I totally agree with you",
+      "don't worry I am here to support you",
+      "that would frustrate me too",
+      "I am on your side",
+      "I understand your frustration",
+    ],
+  },
+  {
+    name: "Symbl.Greetings",
+    vocabulary: [
+      "Good morning",
+      "Hello",
+      "Hi",
+      "How's your day going?",
+      "How are you?",
+      "How are you doing?",
+      "How is it going?",
+    ],
+  },
+  {
+    name: "Symbl.Goodbyes",
+    vocabulary: ["Good bye", "bye", "bye-bye"],
+  },
+  {
+    name: "Product Experience Issue",
+    vocabulary: [
+      "it is not clear what that is",
+      "i probably missed reading everything on the screen before accepting",
+      "i got confused on the app",
+    ],
+  },
+  {
+    name: "Offered Refund",
+    vocabulary: [
+      "offer a full refund",
+      "i would like to offer a full refund",
+      "can I offer you a refund",
+    ],
+  },
+  {
+    name: "Frustration",
+    vocabulary: [
+      "I am really fed up with",
+      "I am very frustrated",
+      "I am not going to talk to another agent",
+      "I want my issue to be fixed right away",
+      "I am not happy",
+      "I am not satisfied",
+      "this is absurd",
+      "really bad service",
+      "poor customer service",
+      "annoying",
+      "I have given up",
+      "got frustrated",
+      "it is a mistake",
+      "it's a mistake",
+    ],
+  },
 ];
 
 module.exports = trackers;
